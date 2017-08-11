2d07564506a399171a0e94062fb1a73706c3b9c8
efournier92 <efournier92@gmail.com>
2017-08-11 18:40:47 -0400

    finish adding logic to get_vid_duration

--- vid_tools.rb
+++ vid_tools.rb
@@ -1,12 +1,12 @@
-
-
-
 def get_vid_duration file_path
-
     string = get_movie_duration(file_path)
+
     hours = string[0,2].to_i
     minutes = string[3,2].to_i
     seconds = string[6,2].to_i
 
+    total_seconds = 0
+    total_seconds = seconds + (minutes * 60) + (hours * 360)
+    total_minutes = total_seconds / 60
 end
 
