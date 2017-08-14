614e500af5d874b48b097862dde1abebddd5308e
efournier92 <efournier92@gmail.com>
2017-08-13 20:01:04 -0400

    add write_concat_files method

--- vid_tools.rb
+++ vid_tools.rb
@@ -1,10 +1,4 @@
 
-def write_concat_files video_file
-  directory = '/Volumes/M_EXTENDED/PRJTS/Opry/GD/EP/'
-  out_directory = '/Volumes/M_EXTENDED/PRJTS/Opry/GD/EP/ls'
-  ep_folders = Dir.entries(directory)
-
-end
 
 def get_movie_duration video_file
   # Run ffmpeg on the video, and do it silently
@@ -13,20 +7,20 @@ def get_movie_duration video_file
   # Find the duration in the output, and force a return if it's found
   duration = /duration: ([0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{2})/i.match(ffmpeg_output) { |m| return m[1] }
 
-  binding.pry
+    binding.pry
   # If it didn't get a match, something is wrong. Log the error
   return "FFMPEG ERROR"
 end
 
 def get_vid_duration file_path
-  string = get_movie_duration(file_path)
+    string = get_movie_duration(file_path)
 
-  hours = string[0,2].to_i
-  minutes = string[3,2].to_i
-  seconds = string[6,2].to_i
+    hours = string[0,2].to_i
+    minutes = string[3,2].to_i
+    seconds = string[6,2].to_i
 
-  total_seconds = 0
-  total_seconds = seconds + (minutes * 60) + (hours * 360)
-  total_minutes = total_seconds / 60
+    total_seconds = 0
+    total_seconds = seconds + (minutes * 60) + (hours * 360)
+    total_minutes = total_seconds / 60
 end
 
